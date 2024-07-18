422f2b981f7aef4134911c2f89b922b5b0d0589c
pollyyana <pollyana.medeirosk@gmail.com>
2024-07-18 17:56:01 -0300

    resolvendo problema web ou nativo

---
+++ lib/rest_client/dio_browser.dart
@@ -0,0 +1,5 @@
+import 'package:dio/browser.dart';
+
+abstract class BaseDio extends DioForBrowser {
+BaseDio(super.options);
+}
--- lib/rest_client/dio_rest_client.dart
+++ lib/rest_client/dio_rest_client.dart
@@ -1,10 +1,14 @@
 import 'package:dio/dio.dart';
-import 'package:dio/io.dart';
+
+//import condicional
+import 'dio_native.dart' if (dart.library.html) 'dio_browser.dart';
 
 //customizar a entrada do Dio pra nao ter que configurar em toda a aplicação o connectTimeout qnd necessario
-class DioRestClient extends DioForNative {
+class DioRestClient extends BaseDio {
   DioRestClient()
       : super(
-          BaseOptions(connectTimeout: Duration(seconds: 10)),
+          BaseOptions(
+            connectTimeout: const Duration(seconds: 10),
+          ),
         );
 }
